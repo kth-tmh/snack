72cb3935609835e44d4d3389dafd62d6e496d26d
Jim O'Regan <joregan@kth.se>
2026-04-13 18:29:05 +0200

    Fix jkFormant prototypes for modern compilers

--- generic/jkFormant.c
+++ generic/jkFormant.c
@@ -436,7 +436,18 @@ Sound *dpform(ps, nform, nom_f1)
 
 #define MAXORDER 30
 
-extern int formant(), lpc(), lpcbsa(), dlpcwtd(), w_covar();
+extern int formant(int lpc_order, double s_freq, double *lpca, int *n_form,
+                   double *freq, double *band, int init);
+extern int lpc(int lpc_ord, double lpc_stabl, int wsize, short *data,
+               double *lpca, double *ar, double *lpck, double *normerr,
+               double *rms, double preemp, int type);
+extern int dlpcwtd(double *s, int *ls, double *p, int *np, double *c,
+                   double *phi, double *shi, double *xl, double *w);
+extern int w_covar(short *xx, int *m, int n, int istrt, double *y,
+                   double *alpha, double *r0, double preemp, int w_type);
+static int lpcbsa(int np, double lpc_stabl, int wind, short *data,
+                  double *lpc, double *rho, double *nul1, double *nul2,
+                  double *energy, double preemp);
 
 /*************************************************************************/
 double integerize(time, freq)
